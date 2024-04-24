7542ea228314cc694280097e7268feff7693b942
Alejandro Aunion Ruiz <127293187+AAunion21@users.noreply.github.com>
2024-04-24 16:33:27 +0200

    Botones Login/Logout

--- app.py
+++ app.py
@@ -34,19 +34,19 @@ tablas.init()
 
 @app.route('/')
 def basic():  # pagina de inicio
-    return render_template('home.html')
+    return render_template('home.html', current_user=current_user)
 
 
 @app.route('/ejercicio4')
 def ej4():  # pagina de inicio del ejercicio 4
-    return render_template('ejercicio4.html')
+    return render_template('ejercicio4.html', current_user=current_user)
 
 
 @app.route('/ejercicio2')
 @login_required
 def ej2():
     data2 = tablas.consultas2()
-    return render_template('ejercicio2.html', **data2)
+    return render_template('ejercicio2.html', **data2, current_user=current_user)
 
 
 @app.route('/ejercicio3')
@@ -54,14 +54,14 @@ def ej2():
 def ej3():
     data3 = ejercicio3.consultas()
     print(data3)
-    return render_template('ejercicio3.html', **data3)
+    return render_template('ejercicio3.html', **data3, current_user=current_user)
 
 
 @app.route('/parte1')
 @login_required
 def ej4_1():
     data4_1 = ejercicio4.ej4_1()
-    return render_template('4parte1.html', **data4_1)
+    return render_template('4parte1.html', **data4_1, current_user=current_user)
 
 
 @app.route('/Ej1parte2', methods=['GET', 'POST'])
@@ -88,35 +88,35 @@ def ej4_2():
                     data4_2 = ejercicio4.ej4_2(usuarios, None)
     else:
         data4_2 = {'df_inseguro': None}
-    return render_template('4parte2.html', **data4_2)
+    return render_template('4parte2.html', **data4_2, current_user=current_user)
 
 
 @app.route('/parte3')
 @login_required
 def ej4_3():
     data4_3 = ejercicio4.ej4_3()
-    return render_template('4parte3.html', **data4_3)
+    return render_template('4parte3.html', **data4_3, current_user=current_user)
 
 
 @app.route('/parte4')
 @login_required
 def ej4_4():
     data4_4 = ejercicio4.ej4_4()
-    return render_template('4parte4.html', **data4_4)
+    return render_template('4parte4.html', **data4_4, current_user=current_user)
 
 
 @app.route('/Ej3parte2')
 @login_required
 def ej3_2():
     data3_2 = parte2Ejercicio3.parte2Ej3()
-    return render_template('3parte2.html', **data3_2)
+    return render_template('3parte2.html', **data3_2, current_user=current_user)
 
 
 @app.route('/Ej4API')
 @login_required
 def ej4_API():
     data4API = ej4API()
-    return render_template('otherAPI.html', **data4API)
+    return render_template('otherAPI.html', **data4API, current_user=current_user)
 
 
 @app.route('/parte1-2', methods=["GET", "POST"])
@@ -131,14 +131,14 @@ def ej1_2():
     else:
         data1_2 = {'html_data': None}
 
-    return render_template('parte2ejercicio1-2.html', **data1_2)
+    return render_template('parte2ejercicio1-2.html', **data1_2, current_user=current_user)
 
 
 @app.route('/ejercicio5', methods=["GET", "POST"])
 @login_required
 def ej5_modelos():
     data5 = ej5()
-    return render_template('ejercicio5.html', **data5)
+    return render_template('ejercicio5.html', **data5, current_user=current_user)
 
 
 @app.route('/procesado', methods=['GET', 'POST'])
@@ -180,7 +180,7 @@ def procesar_datos():
     else:
         prediction = "no crítico"
 
-    return render_template('procesadoExitoso.html', prediction=prediction)
+    return render_template('procesadoExitoso.html', prediction=prediction, current_user=current_user)
 
 
 ###### LOGIN
@@ -207,7 +207,7 @@ def admin_dashboard():
     if not current_user.is_authenticated or not current_user.is_admin():
         abort(403)  # Prohibido
     # Si el usuario es administrador, muestra la pagina del admin
-    return render_template('admin.html')
+    return render_template('admin.html', current_user=current_user)
 
 
 @app.route('/logout')
